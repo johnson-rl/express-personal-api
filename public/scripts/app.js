a552f210f93cfb648526c09226038720dcf7b8e8
johnson-rl <johnson.rl@gmail.com>
2016-12-17 22:06:50 -0800

    plugged in ajax for projects.  Made screenshots the modal button.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,39 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
-// your code
-
+// handlebars function created
+  var source = $('#projects-template').html();
+  var template = Handlebars.compile(source);
+  $.ajax({
+    method: 'GET',
+    url: '/api/projects',
+    success: addProjects,
+    error: onError
+  })
+  function addProjects(json){
+    console.log('ajax got profile projects',json)
+    json.portfolio.forEach(function(project){
+      // console.log(project)
+      var appendHTML = template({json: project})
+      console.log('handlebars',appendHTML)
+      $('#append-projects').append(appendHTML)
+      project.techUsed.forEach(function(technology){
+        $('.'+json._id).append('<li>'+technology+'</li>')
+      })
+    })
+  }
 });
+
+// function addProjects(json){
+//   console.log('ajax got profile projects',json)
+//   json.portfolio.forEach(function(project){
+//     template(project)
+//     project.techUsed.forEach(function(technology){
+//       $('.'+json._id).append('<li>'+technology+'</li>')
+//     })
+//   })
+// }
+
+function onError(json){
+  console.log('ajax experienced and error',json)
+}
--- seed.js
+++ seed.js
@@ -3,20 +3,38 @@
 
 var db = require('./models');
 
-var new_project = {
+var new_project = [{
   name: "Cuddle Dungeon: The Game",
   description: "A final fantasy style game with humor and vicious cuddly creatures.",
   projectLink: "Arrjay.ninja",
   techUsed: ["javascript", "jquery", "bootstrap", "Handlebars"],
-  achievement: "I built the gameboard in jquery, so I will soon be able to create maps of any size!"
-};
+  achievement: "I built the gameboard in jquery, so I will soon be able to create maps of any size!",
+  screenshot: '/images/cuddle-dungeon.png'
+},{
+  name: "My First Portfolio Page",
+  description: "A pretty simple page about me",
+  projectLink: "johnson-rl.github.io",
+  techUsed: ["javascript", "jquery", "bootstrap", "Handlebars"],
+  achievement: "A appended something to the page!  Hey, that was a big deal to me at the time.",
+  screenshot: '/images/portfolio.png'
+},{
+  name: "Tic Tac Toe",
+  description: "My first game.",
+  projectLink: "tics-tacs-and-toes.bitballoon.com",
+  techUsed: ["javascript", "jquery", "bootstrap", "Handlebars"],
+  achievement: "Swapping classes to change colors and determine who won",
+  screenshot: '/images/tic-tac-toe.png'
+}]
 
-db.Project.create(new_project, function(err, project){
-  if (err){
-    return console.log("error:", err);
-  }
-    console.log("Created new project", project._id);
-    process.exit(); // we're all done! Exit the program.
+db.Project.remove({}, function(err, projects){
+  console.log('removed all projects');
+  db.Project.create(new_project, function(err, project){
+    if (err){
+      return console.log("error:", err);
+    };
+      console.log("Created new project", project.length);
+      process.exit(); // we're all done! Exit the program.
+  });
 });
 
 // var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
