dfd2567e2c9372d62cfccbe6290d1720e11bedc1
johnson-rl <johnson.rl@gmail.com>
2016-12-18 14:10:14 -0800

    change project modal built in index.html.  Now hooking up to ajax.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,19 +9,21 @@ $(document).ready(function(){
     url: '/api/projects',
     success: addProjects,
     error: onError
-  })
+  });
   function addProjects(json){
-    console.log('ajax got profile projects',json)
+    // console.log('ajax got profile projects',json)
     json.portfolio.forEach(function(project){
       // console.log(project)
       var appendHTML = template({json: project})
-      console.log('handlebars',appendHTML)
-      $('#append-projects').append(appendHTML)
+      var appendId = project._id
+      // console.log('handlebars',appendHTML)
+      $('#append-projects').append(appendHTML);
       project.techUsed.forEach(function(technology){
-        $('.'+json._id).append('<li>'+technology+'</li>')
-      })
-    })
-  }
+        // console.log(appendId)
+        $('.'+appendId).append('<li>'+technology+'</li>');
+      });
+    });
+  };
 });
 
 // function addProjects(json){
